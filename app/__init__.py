c81174c8ab0d029d38b4edefe849c372cc23a573
artyc99 <artyc99@mail.ru>
2023-04-19 05:08:27 +0300

    base atructure

---
+++ app/__init__.py
@@ -0,0 +1,13 @@
+from flask import Flask
+
+
+class FlaskBot:
+
+    def __init__(self):
+        self.__flask_bot = Flask(__name__)
+
+    def __configurate(self):
+        pass
+
+    def __registrate_blueprints(self):
+        pass
